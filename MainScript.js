9020d798d6e8c8655f1e704f96c0fd40606697d7
Farina Tahsin Chowdhury <farina@Farinas-MacBook-Air.local>
2024-02-06 13:27:51 +0600

    form-fixed

--- MainScript.js
+++ MainScript.js
@@ -3,3 +3,20 @@
 // navigationCheckbox.addEventListener('click', function () {
 //     navigationCheckbox.checked = !navigationCheckbox.checked
 // })
+
+var bookRadioInputSmall = document.getElementById("form-radio-small")
+var bookRadioInputLarge = document.getElementById("form-radio-large")
+var bookRadioToggle = document.getElementById("form__radio-button")
+
+var bgvideo = document.getElementById("bg-video")
+bgvideo.playbackRate = 0.5
+// bookRadioToggle.addEventListener("click", function() {
+//     console.log(bookRadioInputSmall.checked)
+//     if(bookRadioInputSmall.checked === true) {
+//         bookRadioInputSmall.checked = !bookRadioInputSmall.checked
+//     }
+    
+//     console.log(bookRadioInputSmall.checked)
+//     if(!bookRadioInputLarge.checked)
+//         bookRadioInputLarge.checked = !bookRadioInputLarge.checked
+// })
